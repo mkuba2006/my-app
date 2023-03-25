d33b5dcf9c9f17f349847974b5f1b3a3ef636a28
Jakub Myrta <98383163+mkuba2006@users.noreply.github.com>
2023-03-25 12:25:17 +0100

    S

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>123</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>HELLO REACT</h1>, document.getElementById('root'));
