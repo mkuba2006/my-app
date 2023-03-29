4657d3fde78a1d4c89c6ae46013f4450eeedf5b7
Jakub Myrta <98383163+mkuba2006@users.noreply.github.com>
2023-03-29 21:48:27 +0200

    new

---
+++ src/components/plusplus/apl.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Plus(){
+    const [count, next] = React.useState(0);
+
+    function plusplus(){
+        next(count + 1);
+    }
+    function plusminus(){
+        next(count - 1);
+    }
+
+    return(
+        <div className='container'>
+            <h1>{count}</h1>
+            <button onClick={plusplus}>+</button>
+            <button onClick={plusminus}>-</button>
+        </div>
+    );
+}
+
+export default Plus;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header/header.jsx';
 import App from './components/Card/3--app.jsx';
+import Plus from './components/plusplus/apl.jsx';
 
 ReactDOM.render(< Header/>, document.getElementById('root'));
 ReactDOM.render(< App/>, document.getElementById('cont'));
+ReactDOM.render(< Plus/>, document.getElementById('plus'));
